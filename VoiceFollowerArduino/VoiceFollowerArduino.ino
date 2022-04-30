f30609cd833991f3325d801a38821fadacd3ee4b
Muskan <36448298+muskan-0104@users.noreply.github.com>
2022-04-30 09:04:11 +0530

    Add files via upload

---
+++ VoiceFollowerArduino/VoiceFollowerArduino.ino
@@ -0,0 +1,153 @@
+#include <Servo.h>
+#include <Wire.h>
+//#include <ESP8266WiFi.h>
+
+int enA=5;
+int en1=6;
+int en2=7;
+int en3=8;
+int en4=9;
+int enB=10;
+int change=0;
+int c=0;
+Servo myservo;
+String s="";
+
+void setup() {
+
+pinMode(en1,OUTPUT);
+pinMode(en2,OUTPUT);
+pinMode(en3,OUTPUT);
+pinMode(en4,OUTPUT);
+pinMode(enA,OUTPUT);
+pinMode(enB,OUTPUT);
+myservo.attach(11);
+
+ Wire.begin(8);                /* join i2c bus with address 8 */
+ Wire.onReceive(receiveEvent); /* register receive event */
+ 
+ 
+}
+
+void loop()
+{
+      delay(100);
+       if (s=="forward")
+    {
+     forward();
+     s="";
+      }
+     else if (s=="backward")
+    {
+     backward();
+     s="";
+      }
+     else if (s=="left")
+    {
+     left();
+     s="";
+      }
+     else if (s=="right")
+    {
+     right();
+     s="";
+      }
+      else if (s=="stop")
+    {
+     stop();
+     s="";
+      }
+  }
+
+void receiveEvent(int howMany)
+{s="";
+ while (0 <Wire.available()) 
+ {  
+    char c = Wire.read();      /* receive byte as a character */
+    s+=c;
+    Serial.begin(9600);
+    //Serial.println(c);
+  }
+Serial.begin(9600);
+    //Serial.println(s);
+}
+void backward()
+{
+  int var=2;
+    Serial.print(var);
+  analogWrite(enA,255);
+  analogWrite(enB,255);
+  digitalWrite(en1,LOW);
+  digitalWrite(en2,HIGH);
+  digitalWrite(en3,LOW);
+  digitalWrite(en4,HIGH);
+ 
+  
+}
+
+  void forward()
+{
+  int var=1;
+    Serial.print(var);
+    analogWrite(enA,255);
+    analogWrite(enB,255);
+    digitalWrite(en1,HIGH);
+    digitalWrite(en2,LOW);
+    digitalWrite(en3,HIGH);
+    digitalWrite(en4,LOW);
+    
+}
+
+
+ void right()
+{
+  int var=4;
+    Serial.print(var);
+  analogWrite(enA,255);
+  analogWrite(enB,255);
+  digitalWrite(en1,HIGH);
+  digitalWrite(en2,LOW);
+  digitalWrite(en3,LOW);
+  digitalWrite(en4,HIGH);
+  delay(1000);
+     analogWrite(enA,255);
+    analogWrite(enB,255);
+    digitalWrite(en1,HIGH);
+    digitalWrite(en2,LOW);
+    digitalWrite(en3,HIGH);
+    digitalWrite(en4,LOW);
+   
+}
+
+ void left()
+{
+  int var=3;
+    Serial.print(var);
+  analogWrite(enA,255);
+  analogWrite(enB,255);
+  digitalWrite(en1,LOW);
+  digitalWrite(en2,HIGH);
+  digitalWrite(en3,HIGH);
+  digitalWrite(en4,LOW);
+  delay(1000);
+     analogWrite(enA,255);
+    analogWrite(enB,255);
+    digitalWrite(en1,HIGH);
+    digitalWrite(en2,LOW);
+    digitalWrite(en3,HIGH);
+    digitalWrite(en4,LOW);
+   
+}
+ void stop()
+{
+ 
+    int var=0;
+    Serial.print(var);
+    analogWrite(enA,255);
+    analogWrite(enB,255);
+    digitalWrite(en1,LOW);
+    digitalWrite(en2,LOW);
+    digitalWrite(en3,LOW);
+    digitalWrite(en4,LOW);
+}
+
---
+++ VoiceFollowerNodeMcu/VoiceFollowerNodeMcu.ino
@@ -0,0 +1,89 @@
+#include <ESP8266WiFi.h>
+#include  <Wire.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define led            D1
+
+
+#define WLAN_SSID       "Martin Router King"             // Your SSID
+#define WLAN_PASS       "rohan420"                       // Your password
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"                    //Adafruit Server
+#define AIO_SERVERPORT  1883                   
+#define AIO_USERNAME    "rohan00_00"                         // Username
+#define AIO_KEY         "e4323fb63e5940c3ab4254d666dc382f"   // Auth Key
+
+//WIFI CLIENT
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Subscribe Light1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME"/feeds/movecar"); // Feeds name should be same everywhere
+
+
+void MQTT_connect();
+char movestate;
+
+void setup()
+{                                          //Checks wifi status
+  Wire.begin(D1,D2);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+   while (WiFi.status() != WL_CONNECTED)
+    {delay(500);}
+  mqtt.subscribe(&Light1);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+   while (WiFi.status() != WL_CONNECTED) 
+   {delay(500);}
+
+}
+
+
+void loop() 
+{
+  MQTT_connect();
+
+  Adafruit_MQTT_Subscribe *subscription;
+ while ((subscription = mqtt.readSubscription(20000))) //waits 20000 mili sec for subscription message
+  {
+    if (subscription == &Light1)                       //& means that it points to the data stored in Light1 and then subscription has that data???
+    {
+      Serial.begin(9600);
+       //char* movestate=(char *)Light1.lastread;    //atoi converts to an integer value
+       Wire.beginTransmission(8);
+       //Wire.write(movestate);
+       Wire.write((char *)Light1.lastread);//NODEmcu writes the data to arduino
+        Serial.println((char *)Light1.lastread);
+       Wire.endTransmission();
+     }
+  }
+}
+
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  if (mqtt.connected()) {
+    return;
+  }
+  Serial.begin(9600);
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  
+  while ((ret = mqtt.connect()) != 0) {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying MQTT connection in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000); 
+    retries--;
+    if (retries == 0) {
+      while (1);
+    }
+  }
+  Serial.println("MQTT Connected!");
+  
+}
